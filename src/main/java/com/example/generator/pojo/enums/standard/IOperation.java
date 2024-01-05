5a6ed0e840b14a164cc78d9f1980d27fff5e1854
yhl <1004655482@qq.com>
2024-01-05 11:48:57 +0800

    枚举里面实现接口

---
+++ src/main/java/com/example/generator/pojo/enums/standard/IOperation.java
@@ -0,0 +1,24 @@
+package com.example.generator.pojo.enums.standard;
+
+/**
+ * <p>
+ * IOperation描述:计算
+ * <p>
+ * 包名称：com.example.generator.pojo.enums.standard
+ * 类名称：IOperation
+ * 全路径：com.example.generator.pojo.enums.standard.IOperation
+ * 类描述：计算
+ *
+ * @author Administrator-YHL
+ * @date 2024年01月05日 11:45
+ */
+public interface IOperation {
+    /**
+     * 计算
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    double apply(double x, double y);
+}
